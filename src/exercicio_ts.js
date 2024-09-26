c7ae4c615794b36f542dfa162db715f227f10482
rafalves106 <rafalves106@icloud.com>
2024-09-26 18:32:59 -0300

    Subindo projeto

---
+++ src/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+const multiplicaNumeros = (numeroUm, numeroDois) => {
+    return numeroUm * numeroDois;
+};
+console.log(multiplicaNumeros(262, 8.6));
+const retornaFrase = (nome) => {
+    return 'Olá ' + nome;
+};
+console.log(retornaFrase('Pedro'));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,11 @@
+const multiplicaNumeros = (numeroUm: number, numeroDois: number): number => {
+    return numeroUm * numeroDois;
+}
+
+console.log(multiplicaNumeros(262, 8.6));
+
+const retornaFrase = (nome: string): string => {
+    return 'Olá ' + nome;
+}
+
+console.log(retornaFrase('Pedro'));
